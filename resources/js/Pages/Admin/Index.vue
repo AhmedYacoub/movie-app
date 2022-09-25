c1a91c5a1d2b86b3b725d60820a4ac35bc684e8f
Ahmed Yacoub <ahmed.yacoub@outlook.com>
2022-09-25 12:21:35 +0200

    Added AppLayout.vue, Sidebar.vue and Index.vue

---
+++ resources/js/Pages/Admin/Index.vue
@@ -0,0 +1,22 @@
+<script setup>
+// import Welcome from '@/Layouts/Welcome.vue';
+import AdminLayout from "../../Layouts/AdminLayout.vue";
+</script>
+
+<template>
+    <AdminLayout title="Dashboard">
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
+                    <!-- <Welcome /> -->
+                </div>
+            </div>
+        </div>
+    </AdminLayout>
+</template>
