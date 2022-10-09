946749414d83efe633234b3344284a56eda661fe
Ahmed Yacoub <ahmed.yacoub@outlook.com>
2022-10-09 03:31:12 +0200

    Updated TagsController & Tag model. Added in /resources/js/Pages/Admin/Tags the following: 1. Index.vue 2. Create.vue 3. Show.vue

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -14,9 +14,23 @@ namespace App\Models{
 /**
  * App\Models\Cast
  *
+ * @property int $id
+ * @property int $tmdb_id
+ * @property string $name
+ * @property string $slug
+ * @property string $poster_path
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|Cast newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Cast newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Cast query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Cast whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Cast whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Cast whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Cast wherePosterPath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Cast whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Cast whereTmdbId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Cast whereUpdatedAt($value)
  */
 	class Cast extends \Eloquent {}
 }
@@ -25,9 +39,31 @@ namespace App\Models{
 /**
  * App\Models\Episode
  *
+ * @property int $id
+ * @property int $tmdb_id
+ * @property int $season_id
+ * @property string $name
+ * @property string $slug
+ * @property string $overview
+ * @property int $episode_number
+ * @property int $is_public
+ * @property int $visits
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|Episode newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Episode newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Episode query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Episode whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Episode whereEpisodeNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Episode whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Episode whereIsPublic($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Episode whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Episode whereOverview($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Episode whereSeasonId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Episode whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Episode whereTmdbId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Episode whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Episode whereVisits($value)
  */
 	class Episode extends \Eloquent {}
 }
@@ -36,9 +72,21 @@ namespace App\Models{
 /**
  * App\Models\Genre
  *
+ * @property int $id
+ * @property int $tmdb_id
+ * @property string $title
+ * @property string $slug
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|Genre newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Genre newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Genre query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereTmdbId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereUpdatedAt($value)
  */
 	class Genre extends \Eloquent {}
 }
@@ -47,9 +95,41 @@ namespace App\Models{
 /**
  * App\Models\Movie
  *
+ * @property int $id
+ * @property int $tmdb_id
+ * @property string $title
+ * @property string $slug
+ * @property string $overview
+ * @property string $release_date
+ * @property string $runtime
+ * @property string $language
+ * @property string $poster_path
+ * @property string|null $backdrop_path
+ * @property string $video_format
+ * @property int $is_public
+ * @property int $visits
+ * @property string $rating
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|Movie newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Movie newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Movie query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereBackdropPath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereIsPublic($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereLanguage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereOverview($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie wherePosterPath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereRating($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereReleaseDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereRuntime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereTmdbId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereVideoFormat($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Movie whereVisits($value)
  */
 	class Movie extends \Eloquent {}
 }
@@ -58,9 +138,27 @@ namespace App\Models{
 /**
  * App\Models\Season
  *
+ * @property int $id
+ * @property int $tmdb_id
+ * @property int $tv_show_id
+ * @property string $title
+ * @property string $slug
+ * @property int $season_number
+ * @property string $poster_path
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|Season newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Season newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Season query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Season whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Season whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Season wherePosterPath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Season whereSeasonNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Season whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Season whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Season whereTmdbId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Season whereTvShowId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Season whereUpdatedAt($value)
  */
 	class Season extends \Eloquent {}
 }
@@ -69,9 +167,19 @@ namespace App\Models{
 /**
  * App\Models\Tag
  *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|Tag newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Tag newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Tag query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereUpdatedAt($value)
  */
 	class Tag extends \Eloquent {}
 }
@@ -80,9 +188,23 @@ namespace App\Models{
 /**
  * App\Models\Trailer
  *
+ * @property int $id
+ * @property string $name
+ * @property string $embed_html
+ * @property string $trailerable_type
+ * @property int $trailerable_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|Trailer newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Trailer newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Trailer query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Trailer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Trailer whereEmbedHtml($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Trailer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Trailer whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Trailer whereTrailerableId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Trailer whereTrailerableType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Trailer whereUpdatedAt($value)
  */
 	class Trailer extends \Eloquent {}
 }
@@ -91,9 +213,27 @@ namespace App\Models{
 /**
  * App\Models\TvShow
  *
+ * @property int $id
+ * @property int $tmdb_id
+ * @property string $title
+ * @property string $slug
+ * @property string $year_created
+ * @property string $poster_path
+ * @property int $visits
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|TvShow newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|TvShow newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|TvShow query()
+ * @method static \Illuminate\Database\Eloquent\Builder|TvShow whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TvShow whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TvShow wherePosterPath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TvShow whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TvShow whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TvShow whereTmdbId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TvShow whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TvShow whereVisits($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TvShow whereYearCreated($value)
  */
 	class TvShow extends \Eloquent {}
 }
--- app/Http/Controllers/Admin/TagsController.php
+++ app/Http/Controllers/Admin/TagsController.php
@@ -2,13 +2,70 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use Str;
+use App\Models\Tag;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TagsController extends Controller
 {
-    public function index()
+    /**
+     * index
+     *
+     * @return Inertia\Response
+     */
+    public function index(): \Inertia\Response
     {
-        return inertia('Admin/Tags/Index');
+        $tags = Tag::paginate(5);
+
+        return inertia('Admin/Tags/Index')
+            ->with('tags', $tags);
+    }
+
+    /**
+     * show
+     *
+     * @param  Tag $tag
+     * @return Inertia\Response
+     */
+    public function show(Tag $tag): \Inertia\Response
+    {
+        return inertia('Admin/Tags/Show')->with('tag', $tag);
+    }
+
+    /**
+     * create
+     *
+     * @return Inertia\Response
+     */
+    public function create(): \Inertia\Response
+    {
+        return inertia('Admin/Tags/Create');
+    }
+
+    /**
+     * store
+     *
+     * @param  Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:190', 'unique:tags']
+        ], [
+            'name.required' => 'tag name field is required.',
+            'name.unique' => 'the provided tag name already exists.',
+            'name.max' => 'the provided tag name length is too long.'
+        ]);
+
+        $tag = Tag::create([
+            'name' => $request->input('name'),
+            'slug' => Str::slug($request->input('name'))
+        ]);
+
+        return redirect()
+            ->route('admin.tags.index')
+            ->with('flash.banner', 'Tag `'. $tag->name .'` created successfully.');
     }
 }
